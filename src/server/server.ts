61975a74087063102f86703cbc396f24e70e8b47
Rafiere <rafapgbr@gmail.com>
2023-01-06 20:40:08 -0300

    feat: changing initial method message.

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,10 +2,11 @@ import express from "express";
 
 /* Esse arquivo terá o código básico do servidor Express. */
 
+/* Estamos criando uma instância do Express. */
 const server = express();
 
 server.get("/", (req, res) => {
-  res.send("Olá, dev!");
+  res.send({ message: "Olá, dev!" });
 });
 
 export { server };
