214013cf73d20311b9d709e7360631cd62dd5d78
Rafiere <rafapgbr@gmail.com>
2023-01-07 10:02:58 -0300

    feat: creating a route to create a city

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { StatusCodes } from "http-status-codes";
 
+import { CidadesController } from "../controllers";
+
 /* Nesse arquivo, colocaremos todas as rotas do projeto. Esse arquivo é um tipo de "middleware", que informa para o servidor as rotas que ele possui. */
 
 const router = Router();
@@ -8,6 +10,8 @@ const router = Router();
 router.get("/", (req, res) => {
   res.status(StatusCodes.ACCEPTED).json(req.body);
 });
+
+router.post("/cidades", CidadesController.create);
 
 /* Estamos exportando para utilizar as rotas dentro do arquivo de servidor. */
 export { router };
