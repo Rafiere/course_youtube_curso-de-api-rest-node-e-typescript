ea30066711879b415fc1b718a1c32b34018970a8
Rafiere <rafapgbr@gmail.com>
2023-01-07 08:30:26 -0300

    feat: adding env variable

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ import { server } from "./server/server";
 /* Esse será o arquivo principal da aplicação. */
 
 /* Aqui teremos a porta que rodará a aplicação. */
-server.listen(3333, () => console.log("App Rodando!"));
+server.listen(process.env.PORT || 3333, () =>
+  console.log(`App rodando na porta ${process.env.PORT}`)
+);
