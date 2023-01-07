010a65ca4a1f9f2f41492bf8a05b830c10396a65
Rafiere <rafapgbr@gmail.com>
2023-01-07 07:47:04 -0300

    feat: remove routes from server configuration file.

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,17 @@
 import express from "express";
 
+/* Podemos importar diretamente de "routes" pois, quando temos um arquivo chamado "index", o Node já interpreta que estamos chamando o arquivo "index.ts". */
+import { router } from "./routes";
+
 /* Esse arquivo terá o código básico do servidor Express. */
 
 /* Estamos criando uma instância do Express. */
 const server = express();
 
-server.get("/", (req, res) => {
-  res.send({ message: "Olá, dev!" });
-});
+/* Sem informarmos isso, o Express não conseguirá interpretar dados no formato JSON, como, por exemplo, não conseguirá acessar o que está sendo passado no corpo da requisição. */
+server.use(express.json());
+
+/* Estamos pedindo para o servidor reconhecer as rotas da aplicação. */
+server.use(router);
 
 export { server };
